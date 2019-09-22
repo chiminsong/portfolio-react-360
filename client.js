5a29c3d6279e477b6b0eb7580d69b149abd67746
Chimin Song <zerotocoders@gmail.com>
2019-09-23 00:58:24 +0100

    Coffee, Laptop, Spaceship, Planets and PaperPlane added

--- client.js
+++ client.js
@@ -1,7 +1,10 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
+import {Location} from 'react-360-web';
 import {ReactInstance} from 'react-360-web';
+
+// const location = new Location([0, -1, -2]);
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
@@ -10,6 +13,37 @@ function init(bundle, parent, options = {}) {
     ...options,
   });
 
+  const location_coffee = new Location([0, -3, -8]);
+
+  r360.renderToLocation(
+    r360.createRoot('coffee'),
+    location_coffee,
+  );
+
+  const location_laptop = new Location([-5, -3, 0]);
+  r360.renderToLocation(
+    r360.createRoot('laptop'),
+    location_laptop,
+  );
+
+  const location_spaceship = new Location([-10, -3, 5]);
+  r360.renderToLocation(
+    r360.createRoot('spaceship'),
+    location_spaceship,
+  );
+
+  const location_plane = new Location([-5, -3, 0]);
+  r360.renderToLocation(
+    r360.createRoot('plane'),
+    location_plane,
+  );
+
+  const location_planets = new Location([0, -3, 5]);
+  r360.renderToLocation(
+    r360.createRoot('planets'),
+    location_planets,
+  );
+ 
   // Render your app content to the default cylinder surface
   r360.renderToSurface(
     r360.createRoot('test360', { /* initial props */ }),
@@ -17,7 +51,14 @@ function init(bundle, parent, options = {}) {
   );
 
   // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL('nebula_360.jpg'));
+  r360.compositor.setBackground(r360.getAssetURL('background.jpg'));
 }
 
 window.React360 = {init};
+
+
+// Create a location two meters in front of the user, and one meter down
+
+
+
+
--- index.js
+++ index.js
@@ -1,24 +1,201 @@
 import React from 'react';
+import Entity from 'Entity'
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View,
+  asset,
+  AmbientLight,
+  PointLight
 } from 'react-360';
 
-export default class test360 extends React.Component {
+export class test360 extends React.Component {
   render() {
     return (
-      <View style={styles.panel}>
-        <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            leare says hello
-          </Text>
+        <View style={styles.panel}>
+          <View style={styles.greetingBox}>
+            <Text style={styles.greeting}>
+              leare says hello world
+            </Text>
+          </View>
         </View>
-      </View>
     );
   }
 };
+
+export class coffee extends React.Component {
+	render() {
+		return(
+			<View>
+        <AmbientLight intensity={0.7} />
+        <PointLight
+          style={{
+            color: 'white',
+            transform: [
+              {translate: [0, 0, 0]}
+            ]
+          }}
+        />
+        <Entity 
+          source={{
+            obj: asset('coffee.obj'), 
+            mtl: asset('coffee.mtl')
+            }} 
+
+            lit={true}
+            style={{
+              color: "#FFF",
+              transform: [
+                {translate: [-4, 0, 0]},
+                {scale: 0.25}
+              ]
+            }}
+        />
+
+			</View>
+        );
+        
+	}
+}
+
+export class laptop extends React.Component {
+	render() {
+		return(
+			<View>
+        <AmbientLight intensity={0} />
+        <PointLight
+          style={{
+            color: 'white',
+            transform: [
+              {translate: [0, 0, 0]}
+            ]
+          }}
+        />
+        <Entity 
+          source={{
+            obj: asset('laptop.obj'), 
+            mtl: asset('laptop.mtl')
+            }} 
+
+            lit={true}
+            style={{
+              color: "#FFF",
+              transform: [
+                {translate: [-4, 0, 0]},
+                {scale: 0.02}
+              ]
+            }}
+        />
+			</View>
+        );
+	}
+}
+
+export class plane extends React.Component {
+	render() {
+		return(
+			<View>
+        <AmbientLight intensity={0.3} />
+        <PointLight
+          style={{
+            color: 'white',
+            transform: [
+              {translate: [0, 0, 0]}
+            ]
+          }}
+        />
+        <Entity 
+          source={{
+            obj: asset('plane.obj'), 
+            mtl: asset('plane.mtl')
+            }} 
+
+            lit={true}
+            style={{
+              color: "#FFF",
+              transform: [
+                {translate: [30, 0, 0]},
+                {scale: 0.02}
+              ]
+            }}
+        />
+
+			</View>
+        );
+        
+	}
+}
+
+export class spaceship extends React.Component {
+	render() {
+		return(
+			<View>
+        <AmbientLight intensity={0.3} />
+        <PointLight
+          style={{
+            color: 'white',
+            transform: [
+              {translate: [0, 0, 0]}
+            ]
+          }}
+        />
+        <Entity 
+          source={{
+            obj: asset('spaceship.obj'), 
+            mtl: asset('spaceship.mtl')
+            }} 
+
+            lit={true}
+            style={{
+              color: "#FFF",
+              transform: [
+                {translate: [3, 0, 10]},
+                {scale: 3}
+              ]
+            }}
+        />
+
+			</View>
+        );
+        
+	}
+}
+
+export class planets extends React.Component {
+	render() {
+		return(
+			<View>
+        <AmbientLight intensity={-1} />
+        <PointLight
+          style={{
+            color: 'white',
+            transform: [
+              {translate: [0, 0, 0]}
+            ]
+          }}
+        />
+        <Entity 
+          source={{
+            obj: asset('planets.obj'), 
+            mtl: asset('planets.mtl')
+            }} 
+
+            lit={true}
+            style={{
+              color: "#FFF",
+              transform: [
+                {translate: [9, 0, 10]},
+                {scale: 3}
+              ]
+            }}
+        />
+
+			</View>
+        );
+        
+	}
+}
 
 const styles = StyleSheet.create({
   panel: {
@@ -37,6 +214,15 @@ const styles = StyleSheet.create({
   greeting: {
     fontSize: 30,
   },
+  // coffee: {
+  //   width: 10500,
+  //   height: 10500
+  // }
 });
 
-AppRegistry.registerComponent('test360', () => test360);
+AppRegistry.registerComponent('test360',  () => test360);
+AppRegistry.registerComponent('coffee',  () => coffee);
+AppRegistry.registerComponent('laptop',  () => laptop);
+AppRegistry.registerComponent('plane',  () => plane);
+AppRegistry.registerComponent('spaceship',  () => spaceship);
+AppRegistry.registerComponent('planets',  () => planets);
